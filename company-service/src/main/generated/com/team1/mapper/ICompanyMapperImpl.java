4e62e87c45c2daa381b3bc145b302b4662eebb5f
aabdullahaktas <130973437+aabdullahaktas@users.noreply.github.com>
2023-11-03 05:47:14 +0300

    cammit

--- company-service/src/main/generated/com/team1/mapper/ICompanyMapperImpl.java
+++ company-service/src/main/generated/com/team1/mapper/ICompanyMapperImpl.java
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-02T20:11:48+0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
+    date = "2023-11-03T00:09:48+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Amazon.com Inc.)"
 )
 @Component
 public class ICompanyMapperImpl implements ICompanyMapper {
