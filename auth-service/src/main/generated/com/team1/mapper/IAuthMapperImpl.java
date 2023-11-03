fca123e180d49e79e0d6c588a09e3486ba2044b4
Aminenur Göynük <amine.gynk@gmail.com>
2023-11-03 16:24:24 +0300

    Bir seyler silindi

--- auth-service/src/main/generated/com/team1/mapper/IAuthMapperImpl.java
+++ auth-service/src/main/generated/com/team1/mapper/IAuthMapperImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-02T20:13:48+0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
+    date = "2023-11-03T02:02:39+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
 )
 @Component
 public class IAuthMapperImpl implements IAuthMapper {
--- user-service/src/main/generated/com/team1/mapper/IUserMapperImpl.java
+++ user-service/src/main/generated/com/team1/mapper/IUserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-02T14:43:59+0300",
+    date = "2023-11-03T02:02:58+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
 )
 @Component
